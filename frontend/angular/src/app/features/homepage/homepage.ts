7088a9cb5b54d142c9525b29de646add189b07ce
delb-one <delbi92@gmail.com>
2026-07-22 17:43:43 +0200

    add kpi strip section

--- frontend/angular/src/app/features/homepage/homepage.ts
+++ frontend/angular/src/app/features/homepage/homepage.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { Hero } from './sections/hero/hero';
+import { KpiStrip } from "./sections/kpi-strip/kpi-strip";
+import { Services } from "./sections/services/services";
+import { HowItWorks } from "./sections/how-it-works/how-it-works";
+import { Faq } from "./sections/faq/faq";
+import { Contacts } from "./sections/contacts/contacts";
 
 @Component({
   selector: 'app-homepage',
-  imports: [Hero],
+  imports: [Hero, KpiStrip, Services, HowItWorks, Faq, Contacts],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
--- frontend/angular/src/app/features/homepage/sections/kpi-strip/kpi-strip.ts
+++ frontend/angular/src/app/features/homepage/sections/kpi-strip/kpi-strip.ts
@@ -1,9 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-kpi-strip',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './kpi-strip.html',
   styleUrl: './kpi-strip.css',
 })
-export class KpiStrip {}
+export class KpiStrip {
+  stats = [
+    {
+      value: '6',
+      label: 'Categorie di segnalazione',
+    },
+    {
+      value: '5',
+      label: 'Uffici competenti',
+    },
+    {
+      value: '6',
+      label: 'Fasi di lavorazione',
+    },
+    {
+      value: '2026',
+      label: 'Servizio attivo dal',
+    },
+  ];
+}
