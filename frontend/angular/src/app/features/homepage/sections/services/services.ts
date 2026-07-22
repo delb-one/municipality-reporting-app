cc9564c083c629bda9f5ef517cd01256cf02c9f1
delb-one <delbi92@gmail.com>
2026-07-22 17:47:06 +0200

    add services section

--- frontend/angular/src/app/features/homepage/sections/services/services.ts
+++ frontend/angular/src/app/features/homepage/sections/services/services.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ItIconComponent, ItCardComponent } from "design-angular-kit";
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [ItIconComponent, ItCardComponent],
   templateUrl: './services.html',
   styleUrl: './services.css',
 })
